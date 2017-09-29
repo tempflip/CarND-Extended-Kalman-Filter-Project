34a77a0c9e6589959d46580efcdf35c14e374776
Peter Tempfli <tempflip@gmail.com>
2017-09-29 09:54:56 +0200

    w

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,8 @@ std::string hasData(std::string s) {
 
 int main()
 {
+  cout << "Hello we are starting!!!!" << endl;
+
   uWS::Hub h;
 
   // Create a Kalman Filter instance
@@ -43,30 +45,28 @@ int main()
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
 
-    if (length && length > 2 && data[0] == '4' && data[1] == '2')
-    {
-
+    if (length && length > 2 && data[0] == '4' && data[1] == '2') {
       auto s = hasData(std::string(data));
       if (s != "") {
-      	
         auto j = json::parse(s);
-
         std::string event = j[0].get<std::string>();
         
         if (event == "telemetry") {
           // j[1] is the data JSON object
           
+
           string sensor_measurment = j[1]["sensor_measurement"];
-          
+          cout << "IN: " << sensor_measurment << endl;
+
           MeasurementPackage meas_package;
           istringstream iss(sensor_measurment);
-    	  long long timestamp;
+          long long timestamp;
 
-    	  // reads first element from the current line
-    	  string sensor_type;
-    	  iss >> sensor_type;
+      	  // reads first element from the current line
+      	  string sensor_type;
+      	  iss >> sensor_type;
 
-    	  if (sensor_type.compare("L") == 0) {
+          if (sensor_type.compare("L") == 0) {
       	  		meas_package.sensor_type_ = MeasurementPackage::LASER;
           		meas_package.raw_measurements_ = VectorXd(2);
           		float px;
@@ -90,56 +90,70 @@ int main()
           		iss >> timestamp;
           		meas_package.timestamp_ = timestamp;
           }
+
           float x_gt;
-    	  float y_gt;
-    	  float vx_gt;
-    	  float vy_gt;
-    	  iss >> x_gt;
-    	  iss >> y_gt;
-    	  iss >> vx_gt;
-    	  iss >> vy_gt;
-    	  VectorXd gt_values(4);
-    	  gt_values(0) = x_gt;
-    	  gt_values(1) = y_gt; 
-    	  gt_values(2) = vx_gt;
-    	  gt_values(3) = vy_gt;
-    	  ground_truth.push_back(gt_values);
+          float y_gt;
+          float vx_gt;
+       	  float vy_gt;
+       	  iss >> x_gt;
+       	  iss >> y_gt;
+       	  iss >> vx_gt;
+       	  iss >> vy_gt;
+       	  VectorXd gt_values(4);
+       	  gt_values(0) = x_gt;
+       	  gt_values(1) = y_gt; 
+       	  gt_values(2) = vx_gt;
+       	  gt_values(3) = vy_gt;
+
+      	  ground_truth.push_back(gt_values);
           
           //Call ProcessMeasurment(meas_package) for Kalman filter
-    	  fusionEKF.ProcessMeasurement(meas_package);    	  
+          fusionEKF.ProcessMeasurement(meas_package);    	  
 
-    	  //Push the current estimated x,y positon from the Kalman filter's state vector
+      	  //Push the current estimated x,y positon from the Kalman filter's state vector
+      	  VectorXd estimate(4);
+      	  double p_x = fusionEKF.ekf_.x_(0);
+      	  double p_y = fusionEKF.ekf_.x_(1);
+      	  double v1  = fusionEKF.ekf_.x_(2);
+      	  double v2 = fusionEKF.ekf_.x_(3);
+      	  
+          estimate(0) = p_x;
+      	  estimate(1) = p_y;
+      	  estimate(2) = v1;
+      	  estimate(3) = v2;
+      	  
+      	  estimations.push_back(estimate);
 
-    	  VectorXd estimate(4);
-
-    	  double p_x = fusionEKF.ekf_.x_(0);
-    	  double p_y = fusionEKF.ekf_.x_(1);
-    	  double v1  = fusionEKF.ekf_.x_(2);
-    	  double v2 = fusionEKF.ekf_.x_(3);
-
-    	  estimate(0) = p_x;
-    	  estimate(1) = p_y;
-    	  estimate(2) = v1;
-    	  estimate(3) = v2;
-    	  
-    	  estimations.push_back(estimate);
-
-    	  VectorXd RMSE = tools.CalculateRMSE(estimations, ground_truth);
+          // RMSE
+          VectorXd RMSE = tools.CalculateRMSE(estimations, ground_truth);
 
           json msgJson;
+          
+          /*
           msgJson["estimate_x"] = p_x;
           msgJson["estimate_y"] = p_y;
           msgJson["rmse_x"] =  RMSE(0);
           msgJson["rmse_y"] =  RMSE(1);
           msgJson["rmse_vx"] = RMSE(2);
           msgJson["rmse_vy"] = RMSE(3);
+          */
+
+          // DUMMY MSG
+          ///////////////////
+          msgJson["estimate_x"] = 1;
+          msgJson["estimate_y"] = 2;
+          msgJson["rmse_x"] =  3;
+          msgJson["rmse_y"] =  4;
+          msgJson["rmse_vx"] = 5;
+          msgJson["rmse_vy"] = 6;
+          ////////////////////
+          //////////////////
+
           auto msg = "42[\"estimate_marker\"," + msgJson.dump() + "]";
-          // std::cout << msg << std::endl;
+          std::cout << "OUT: " << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-	  
-        }
+        } // if 'telemetry' event end
       } else {
-        
         std::string msg = "42[\"manual\",{}]";
         ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
       }
